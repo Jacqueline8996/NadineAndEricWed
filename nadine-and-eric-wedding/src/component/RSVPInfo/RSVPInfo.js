d8f253902d8d11818926ab937eadfdae20cc439b
Jacqueline Rivera <Jacqueline8996@gmail.com>
2021-08-30 21:50:46 -0400

    fixxed mobile responsive

--- nadine-and-eric-wedding/src/component/RSVPInfo/RSVPInfo.js
+++ nadine-and-eric-wedding/src/component/RSVPInfo/RSVPInfo.js
@@ -7,27 +7,27 @@ function RSVPInfo() {
   return (
     <div>
       <div className="container divCenter" >
-        <div>
-          <p className="headdingR">Welcome!</p>
-          <p className="headdingR">Thank you for being part of our special day</p>
-          <div className="col-12" >
-            <img src={NadinePic} alt = "nadine and eric" className = "coverImage"></img>
+        <div className="row align-self-center">
+          <div className="col-md-12" >
+            <div className="col-md-12" >
+              <p className="headdingR">Welcome!</p>
+              <p className="headdingR">Thank you for being part of our special day</p>
+              <img src={NadinePic} alt = "nadine and eric" className = "coverImage"></img>
           </div>
-          <section>
-              <div className="row align-self-center">
-                  <div className="col-md-12 ">
-                      <p id="rvIn">RSVP Info</p>
-                  </div>
-                  <div className="col-md-12 BodyOfTextR">
-                      <p>
-                        Please click on the following link to RSVP via google:
-                      </p>
-                      <a className="clickRsvp" href="https://forms.gle/zfWr17GTrhabgiEM7">
-                        Click me to RSVP ♥
-                      </a>
-                  </div>
-              </div>
-          </section>
+            <div className="row align-self-center">
+                <div className="col-md-12">
+                    <p className="headding" id="rvIn">RSVP Info</p>
+                </div>
+                <div className="col-md-12 BodyOfTextR">
+                    <p>
+                      Please click on the following link to RSVP via google:
+                    </p>
+                    <a className="clickRsvp" href="https://forms.gle/zfWr17GTrhabgiEM7">
+                      Click me to RSVP ♥
+                    </a>
+                </div>
+            </div>
+        </div>
         </div>
       </div>
     </div>
